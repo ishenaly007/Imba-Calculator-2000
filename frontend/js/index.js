815980f40e6be9bbb412cc50698c24583911b754
Tilek Erkebaev <108805434+Tilekdevs@users.noreply.github.com>
2025-02-17 14:47:25 +0600

    add: more easter

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,35 +1,42 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
+let isEasterEggActive = false; 
+
 buttons.forEach((item) => {
   item.onclick = () => {
     if (item.id == "clear") {
       display.innerText = "";
+      isEasterEggActive = false; 
     } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
+      display.innerText = display.innerText.slice(0, -1);
+      if (!isNaN(display.innerText)) isEasterEggActive = false; 
     } else if (display.innerText != "" && item.id == "equal") {
       const expression = display.innerText.trim();
-
+      
       const easterEggs = {
         "2+2": "Слишком сложно...",
-        "1+1": "Тупооой...",
-        "1000-7": "дед инсайд(",
+        "1+1": "Тупоооой...",
+        "007": "Агент Бонд",
+        "1000-7": "Дед инсайд(",
       };
 
       if (easterEggs.hasOwnProperty(expression)) {
         display.innerText = easterEggs[expression];
+        isEasterEggActive = true;
       } else {
         display.innerText = eval(expression);
       }
     } else if (display.innerText == "" && item.id == "equal") {
       display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 1000);
+      setTimeout(() => (display.innerText = ""), 2000);
     } else {
+      if (isEasterEggActive && !isNaN(item.id)) return;
       display.innerText += item.id;
     }
   };
 });
+
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
