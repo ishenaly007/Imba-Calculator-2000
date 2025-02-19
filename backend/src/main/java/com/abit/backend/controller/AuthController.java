da5e044f422b506cf99c33ab58273ebc7ca29681
Tilek Erkebaev <108805434+Tilekdevs@users.noreply.github.com>
2025-02-19 17:39:37 +0600

    error: cors

--- backend/src/main/java/com/abit/backend/controller/AuthController.java
+++ backend/src/main/java/com/abit/backend/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/test/login")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AuthController {
     private final UserService userService;
 
--- backend/src/main/java/com/abit/backend/controller/UserController.java
+++ backend/src/main/java/com/abit/backend/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/test/users")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
 
